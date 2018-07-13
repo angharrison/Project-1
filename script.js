2418609d6c5053ce86be96cb2dec36fb040c27b1
Angelica Harrison <harrison_angelica@yahoo.com>
2018-07-13 11:43:26 -0400

    making clicks

--- script.js
+++ script.js
@@ -1,34 +1,78 @@
-
-const tower = document.querySelectorAll('.tower')
+const tower1 = document.querySelector('#first')
+const tower2 = document.querySelector('#second')
+const tower3 = document.querySelector('#third')
 const diskSelector = 3
 const diskRule = []
+
 //DOM Elements
     for (i = 1; i <= diskSelector; i++) {
       let diskDiv = document.createElement('div')
-      diskDiv.id = "disk" + [i]
+      diskDiv.id = "disk" + i
       diskDiv.className = 'disk'
-      tower[0].appendChild(diskDiv)
+      tower1.appendChild(diskDiv)
       diskRule.push(diskDiv.id)
     }
 
 
+//Highlighting
+let active = false
+
+let actions = function(){
+  if(active === false && this.childElementCount === 0){
+    return
+  }else if (active === this.lastChild){
+    this.lastChild.style.borderWidth = '1px'
+    active = false
+  }else if (active === false){
+    this.lastChild.style.borderWith = '5px'
+    active = this.lastChild
+  }else if (active.offsetWidth < this.lastChild.offsetWidth || this.childElementCount === 0){
+    active.style.borderWidth = '1px'
+    this.appendChild(active)
+    winner()
+    active = false
+  }else{
+    active.style.borderWith = '1px'
+    active = false}
+    }
+
+
+  tower1.addEventListener('click', actions)
+  tower2.addEventListener('click', actions)
+  tower3.addEventListener('click', actions)
+
+
+
+
+
+
+
+
+
+
+
+
+  var para = document.createElement("P");
+  var t = document.createTextNode("Game Rules");
+  para.appendChild(t);
+  document.getElementById("button1").appendChild(para);
+  id="button1"
+
+
+
+
 
 //Hidden Button
+/*
+
 var text = document.getElementById("text");
 
-var button = document.getElementById("reveal");
+var button = document.getElementById("button1");
 
 button.onclick = function () {
   text.style.display = "inline";
-  //you can write another function or toggle between two behaviors
-}
-
-//document on debugger console will display whole document
-//button = variable button (info) container
-// text refers to variable we created in line 1
-// text.style.display (we are changing the style of the text element tot go in-line)
-
 
   function myFunction() {
-    document.getElementById("demo").innerHTML = "Restart game";
+    document.getElementById("button2").innerHTML = "Restart game";
   }
+*/
