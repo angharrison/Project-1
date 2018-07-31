8e0b1a6bd561062cd34287ab4050b59289dfa7c0
Angelica Harrison <harrison_angelica@yahoo.com>
2018-07-31 17:49:44 -0400

    comments in code

--- script.js
+++ script.js
@@ -1,34 +1,43 @@
+const tower1 = document.querySelector('#first')                                           //identify and constant each tower
+const tower2 = document.querySelector('#second')
+const tower3 = document.querySelector('#third')
+let diskSelector = 4
 
-const tower = document.querySelectorAll('.tower')
-const diskSelector = 3
-const diskRule = []
 //DOM Elements
-    for (i = 1; i <= diskSelector; i++) {
-      let diskDiv = document.createElement('div')
-      diskDiv.id = "disk" + [i]
-      diskDiv.className = 'disk'
-      tower[0].appendChild(diskDiv)
-      diskRule.push(diskDiv.id)
+    for (i = 1; i <= diskSelector; i++){                                                     //for loop for disks
+      let diskDiv = document.createElement('div')                                            //create diskDiv to hold all disks
+      diskDiv.id = "disk" + i                                                                  //touch css elements and ability to add one disk to diskdiv
+      diskDiv.className = 'disk'                                                                //touch html element with individual disks
+      tower1.appendChild(diskDiv)                                                          //attach children disks to tower 1
     }
 
+//Highlighting
+let active = false                                                                       //highlighting is off to start
 
-
-//Hidden Button
-var text = document.getElementById("text");
-
-var button = document.getElementById("reveal");
-
-button.onclick = function () {
-  text.style.display = "inline";
-  //you can write another function or toggle between two behaviors
+let actions = function(){                                                            //create function to enable movement for towers
+   if (active === false && this.childElementCount === 0){                            //if highlighting is off and there are no disks in the tower, no reaction
+       return
+   }else if (active === false){                                                   //if highlighting is off
+       this.lastChild.style.borderWidth = '5px'                                      //the border of lastChild(last disk selected) is highlighted by clicking on tower
+       active = this.lastChild                                                               //last clicked disk becomes active
+   }else if (this.childElementCount === 0 || active.offsetWidth < this.lastChild.offsetWidth) { //if tower is empty of disks or width of active (selected) disk is less than width of uppermost disk on tower, no movement. offsetWidth is the size of the visual box w/o borders so it doesn't include highlighted border/margin/scroll tab. keeps the rule bigger disks aren't put on top of smaller disks
+       active.style.borderWidth = '1px'                                                     //active is last disk and border is unhighlighted to enable movement
+       this.appendChild(active)                                                           //active(last selected disk) is moved and attached to element tower clicked on
+       active = false                                                                 //unhighlight border of disk once moved to another tower
+       winner()
+   }else{
+       active.style.borderWidth = '1px'                                                   //if all else, active disk is unhighlighted 
+       active = false
+     }
 }
 
-//document on debugger console will display whole document
-//button = variable button (info) container
-// text refers to variable we created in line 1
-// text.style.display (we are changing the style of the text element tot go in-line)
+tower1.addEventListener('click', actions)                                                   //click for each tower
+tower2.addEventListener('click', actions)
+tower3.addEventListener('click', actions)
 
 
-  function myFunction() {
-    document.getElementById("demo").innerHTML = "Restart game";
-  }
+
+  let btnr = document.querySelector("#button2")                                                    //clear button to reset game
+  btnr.addEventListener('click', () => {
+    location.reload()
+  })
